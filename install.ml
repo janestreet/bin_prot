8044afb2330e4e3b0002305c7588834c6569b6b8
Jeremie Dimino <jdimino@janestreet.com>
2016-04-26 17:03:46 +0100

    113.33.03

---
+++ install.ml
@@ -0,0 +1,9 @@
+#use "topfind";;
+#require "js-build-tools.oasis2opam_install";;
+
+open Oasis2opam_install;;
+
+generate ~package:"bin_prot"
+  [ oasis_lib "bin_prot"
+  ; file "META" ~section:"lib"
+  ]
--- js-utils/install_tags.ml
+++
@@ -1,9 +0,0 @@
-let package_name = "bin_prot"
-
-let sections =
-  [ ("lib",
-    [ ("built_lib_bin_prot", None)
-    ],
-    [ ("META", None)
-    ])
-  ]
