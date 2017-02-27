2999378389c86acb06ffd7914e5dfbbab3f1f1bf
Jeremie Dimino <jdimino@janestreet.com>
2017-02-27 15:54:03 +0000

    114.33+30

--- xen/float_stubs.c
+++ xen/float_stubs.c
@@ -1,1 +1,7 @@
-../src/float_stubs.c
+#include <caml/mlvalues.h>
+#include <caml/bigarray.h>
+
+CAMLprim value bin_prot_get_float_offset(value buf, value pos)
+{
+  return (value)((char *)Caml_ba_data_val(buf) + Long_val(pos));
+}
